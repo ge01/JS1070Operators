99b606924e6ef9b0fb0920bcb04d5a9efd8ccb31
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-06-27 12:16:46 -0500

    JavaScript Operators

---
+++ app.js
@@ -0,0 +1,14 @@
+var x = 5;
+var y = 2;
+var z = x + y;
+document.getElementById("demo").innerHTML = z;
+
+txt1 = "What a very ";
+txt1 += "nice day";
+document.getElementById("demo1").innerHTML = txt1;
+
+var x = 5 + 5;
+var y = "5" + 5;
+var z = "Hello" + 5;
+document.getElementById("demo2").innerHTML =
+x + "<br>" + y + "<br>" + z;
